35e0847dfd1dc0785efe05dd0f15c9dbbac2fabd
Eric Kimball <Eric-k93@live.com>
2018-06-06 07:43:24 -0700

    Prune, 8ball, ping working

--- src/index.js
+++ src/index.js
@@ -1,47 +1,130 @@
-const Discord = require('discord.js');
+const Discord = require("discord.js");
+const client = new Discord.Client();
+const config = require("./config.json");
 
-const bot = new Discord.Client();
+const token = 'NDUxNjQyNDI2NTIwNTAyMjcz.DfExrA.IGwQnaSqmS28aBPD21BIx0AjQAk'
+const myId = '169678656195526657';
+const prefix = '!';
 
-const token = 'NDUxNjQyNDI2NTIwNTAyMjcz.DfExrA.IGwQnaSqmS28aBPD21BIx0AjQAk';
-const myId = '451642426520502273';
+const eightBall = [
+    'Yes',
+    'Maybe',
+    'Unsure',
+    'Try again',
+    'Signs point to yes',
+    'You don\'t want to know',
+    'You can try...'
+]
+
 const responseObject = {
     ".stats": "OMEGALUL",
     "kid": "kid in *current year*, LUL",
     "overwatch": "People still play overwatch? Trihard 7",
     "ow": "People still play overwatch? Trihard 7"
 }
-bot.on('ready', () => {
-    console.log('Ready to bm!');
+
+client.on('ready', () => {
+    console.log(`Bot has started, with ${client.users.size} users, in ${client.channels.size} channels of ${client.guilds.size} guilds.`); 
+    client.user.setActivity(`Serving ${client.guilds.size} servers`);
+});
+
+client.on("guildCreate", guild => {
+    // This event triggers when the bot joins a guild.
+    console.log(`New guild joined: ${guild.name} (id: ${guild.id}). This guild has ${guild.memberCount} members!`);
+    client.user.setActivity(`Serving ${client.guilds.size} servers`);
+  });
+
+client.on("guildDelete", guild => {
+// this event triggers when the bot is removed from a guild.
+console.log(`I have been removed from: ${guild.name} (id: ${guild.id})`);
+client.user.setActivity(`Serving ${client.guilds.size} servers`);
 });
 
 
-bot.on('message', message => {
-    // No self-reply
-    if (message.author.bot) return;
 
-    //Check for command and stats +target
-    if (message.content.includes('ez')) {
-        var stat = '.stats'
-        var lul = 'OMEGALUL'
-        message.channel.send(stat + lul)
+client.on('message', async message => {
+    if (message.author.bot) return; // ignore commands sent by bots.
 
+    if(message.content.indexOf(config.prefix) !== 0) return;
+  
+    // Here we separate our "command" name, and our "arguments" for the command. 
+    // e.g. if we have the message "+say Is this the real life?" , we'll get the following:
+    // command = say
+    // args = ["Is", "this", "the", "real", "life?"]
+    const args = message.content.slice(prefix.length).trim().split(' ');
+    const command = args[0].toLowerCase();
+
+    if(command == "ping") {
+        // Calculates ping between sending a message and editing it, giving a nice round-trip latency.
+        // The second ping is an average latency between the bot and the websocket server (one-way, not round-trip)
+        const m = await message.channel.send("Ping?");
+        m.edit(`Pong! Latency is ${m.createdTimestamp - message.createdTimestamp}ms. API Latency is ${Math.round(client.ping)}ms`);
+      }
+
+
+    if (command == "cookie") { // creates the command cookie
+        if (args[1]) message.channel.send(message.author.toString() + " has given " + args[1].toString() + " a cookie! :cookie:") // sends the message saying someone has given someone else a cookie if someone mentions someone else
+        else message.channel.send("Who do you want to send a cookie to? :cookie: (Correct usage: !cookie @username)") // sends the error message if no-one is mentioned
     }
-    if (message.author.id == myId){
-        // Purge function from github --> "https://github.com/AnIdiotsGuide/discordjs-bot-guide/blob/master/examples/miscellaneous-examples.md"
-        const user = message.mentions.users.first();
-        const amount = !!parseInt(message.content.split(' ')[1]) ? parseInt(message.content.split(' ')[1]) : parseInt(message.content.split(' ')[2])
-        if (!amount) return message.reply('Must specify an amount to delete!');
-        if (!amount && !user) return message.reply('Must specify a user and amount, or just an amount, of messages to purge!');
-        message.channel.fetchMessages({
-        limit: amount,
-        }).then((messages) => {
-        if (user) {
-        const filterBy = user ? user.id : Client.user.id;
-        messages = messages.filter(m => m.author.id === filterBy).array().slice(0, amount);
+
+    if (command == "8ball") { // creates the command 8ball
+        if (args[1] != null) message.reply(eightball[Math.floor(Math.random() * eightball.length).toString(16)]); // if args[1], post random answer
+        else message.channel.send("Ummmm, what is your question? :rolling_eyes: (Correct usage: *8ball [question])"); // if not, error
+    }
+
+    if(command === "purge") {
+        var isNum = false;
+        if (!isNaN(args[1])) {
+            let messagecount = parseInt(args[1]);
+            isNum = true;
+        } else {
+            let messagecount = parseInt(args[2]);
         }
-        message.channel.bulkDelete(messages).catch(error => console.log(error.stack));
-        });
+
+        var deletedMessages = -1;
+
+        message.channel.fetchMessages({limit: Math.min(messagecount + 1, 100)}).then(messages => {
+            if (isNum == true) {
+                messages.forEach(m => {
+                    m.delete().catch(console.error);
+                    deletedMessages++;
+                });
+            } else {
+                messages.forEach(m => {
+                    if (m.author.id == args[1].toString) {
+                        m.delete().catch(console.error);
+                        deletedMessages++;
+                    }
+                });
+            }
+        }).then(() => {
+                if (deletedMessages === -1) deletedMessages = 0;
+                message.channel.send(`:white_check_mark: Purged \`${deletedMessages}\` messages.`)
+                    .then(m => m.delete(2000));
+        }).catch(console.error);
+
     }
+
+    if(command === "botpurge") {
+        let messagecount = parseInt(args[1]) || 1;
+
+        var deletedMessages = -1;
+
+        message.channel.fetchMessages({limit: Math.min(messagecount + 1, 100)}).then(messages => {
+            messages.forEach(m => {
+                if (m.author.id == 451642426520502273) {
+                    m.delete().catch(console.error);
+                    deletedMessages++;
+                }
+            });
+        }).then(() => {
+                if (deletedMessages === -1) deletedMessages = 0;
+                message.channel.send(`:white_check_mark: Purged \`${deletedMessages}\` messages.`)
+                    .then(m => m.delete(2000));
+        }).catch(console.error);
+    }
+
+
 
 // If a message includes any commands from the array, post the value associated with it.
 
@@ -49,8 +132,9 @@ bot.on('message', message => {
         message.channel.send(responseObject[message.content]);
     }
 
+
 });
 
 
 
-bot.login(token);
+client.login(token);
