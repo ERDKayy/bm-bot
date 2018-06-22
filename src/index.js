06bb0830e7ce2606667d5b63d433d9f6a2d8bee0
unknown <eric-k93@live.com>
2018-06-21 18:25:55 -0700

    added TODO for image prune

--- src/index.js
+++ src/index.js
@@ -51,6 +51,12 @@ client.user.setActivity(`Serving ${client.guilds.size} servers`);
 client.on('message', async message => {
     if (message.author.bot) return; // ignore commands sent by bots.
 
+    // Catch images posted in #General chat
+    // TODO: 
+    //      -> Catch images
+    //      -> Delete image post
+    //      -> warn user
+    //      -> repost image in pics_and_clips
     if(message.attachments != 0) {
         console.log(message.attachments)
         console.log('Caught an embeded IMAGE!!!!!!');
@@ -59,96 +65,96 @@ client.on('message', async message => {
     }
 
 
-    if(message.content.indexOf(config.prefix) !== 0) return;
-  
-    // Here we separate our "command" name, and our "arguments" for the command. 
-    // e.g. if we have the message "+say Is this the real life?" , we'll get the following:
-    // command = say
-    // args = ["Is", "this", "the", "real", "life?"]
-    const args = message.content.slice(prefix.length).trim().split(' ');
-    const command = args[0].toLowerCase();
+    if(message.content.indexOf(config.prefix) == 0) {
+
+        // Here we separate our "command" name, and our "arguments" for the command. 
+        // e.g. if we have the message "+say Is this the real life?" , we'll get the following:
+        // command = say
+        // args = ["Is", "this", "the", "real", "life?"]
+        const args = message.content.slice(prefix.length).trim().split(' ');
+        const command = args[0].toLowerCase();
 
 
 
-    if(command == "ping") {
-        console.log('test')
-        // Calculates ping between sending a message and editing it, giving a nice round-trip latency.
-        // The second ping is an average latency between the bot and the websocket server (one-way, not round-trip)
-        const m = await message.channel.send("Ping?");
-        m.edit(`Pong! Latency is ${m.createdTimestamp - message.createdTimestamp}ms. API Latency is ${Math.round(client.ping)}ms`);
-      }
-
-
-    if (command == "cookie") { // creates the command cookie
-        if (args[1]) message.channel.send(message.author.toString() + " has given " + args[1].toString() + " a cookie! :cookie:") // sends the message saying someone has given someone else a cookie if someone mentions someone else
-        else message.channel.send("Who do you want to send a cookie to? :cookie: (Correct usage: !cookie @username)") // sends the error message if no-one is mentioned
-    }
-
-    if (command == "8ball") { // creates the command 8ball
-        if (args[1] != null) message.reply(eightball[Math.floor(Math.random() * eightball.length).toString(16)]); // if args[1], post random answer
-        else message.channel.send("Ummmm, what is your question? :rolling_eyes: (Correct usage: *8ball [question])"); // if not, error
-    }
-
-    if(command === "purge") {
-        var isNum = false;
-        if (!isNaN(args[1])) {
-            let messagecount = parseInt(args[1]);
-            isNum = true;
-        } else {
-            let messagecount = parseInt(args[2]);
+        if(command == "ping") {
+            console.log('test')
+            // Calculates ping between sending a message and editing it, giving a nice round-trip latency.
+            // The second ping is an average latency between the bot and the websocket server (one-way, not round-trip)
+            const m = await message.channel.send("Ping?");
+            m.edit(`Pong! Latency is ${m.createdTimestamp - message.createdTimestamp}ms. API Latency is ${Math.round(client.ping)}ms`);
         }
 
-        var deletedMessages = -1;
 
-        message.channel.fetchMessages({limit: Math.min(messagecount + 1, 100)}).then(messages => {
-            if (isNum == true) {
-                messages.forEach(m => {
-                    m.delete().catch(console.error);
-                    deletedMessages++;
-                });
+        if (command == "cookie") { // creates the command cookie
+            if (args[1]) message.channel.send(message.author.toString() + " has given " + args[1].toString() + " a cookie! :cookie:") // sends the message saying someone has given someone else a cookie if someone mentions someone else
+            else message.channel.send("Who do you want to send a cookie to? :cookie: (Correct usage: !cookie @username)") // sends the error message if no-one is mentioned
+        }
+
+        if (command == "8ball") { // creates the command 8ball
+            if (args[1] != null) message.reply(eightball[Math.floor(Math.random() * eightball.length).toString(16)]); // if args[1], post random answer
+            else message.channel.send("Ummmm, what is your question? :rolling_eyes: (Correct usage: *8ball [question])"); // if not, error
+        }
+
+        if(command === "purge") {
+            var isNum = false;
+            if (!isNaN(args[1])) {
+                let messagecount = parseInt(args[1]);
+                isNum = true;
             } else {
+                let messagecount = parseInt(args[2]);
+            }
+
+            var deletedMessages = -1;
+
+            message.channel.fetchMessages({limit: Math.min(messagecount + 1, 100)}).then(messages => {
+                if (isNum == true) {
+                    messages.forEach(m => {
+                        m.delete().catch(console.error);
+                        deletedMessages++;
+                    });
+                } else {
+                    messages.forEach(m => {
+                        if (m.author.id == args[1].toString) {
+                            m.delete().catch(console.error);
+                            deletedMessages++;
+                        }
+                    });
+                }
+            }).then(() => {
+                    if (deletedMessages === -1) deletedMessages = 0;
+                    message.channel.send(`:white_check_mark: Purged \`${deletedMessages}\` messages.`)
+                        .then(m => m.delete(2000));
+            }).catch(console.error);
+
+        }
+
+        if(command === "botpurge") {
+            let messagecount = parseInt(args[1]) || 1;
+
+            var deletedMessages = -1;
+
+            message.channel.fetchMessages({limit: Math.min(messagecount + 1, 100)}).then(messages => {
                 messages.forEach(m => {
-                    if (m.author.id == args[1].toString) {
+                    if (m.author.id == 451642426520502273) {
                         m.delete().catch(console.error);
                         deletedMessages++;
                     }
                 });
-            }
-        }).then(() => {
-                if (deletedMessages === -1) deletedMessages = 0;
-                message.channel.send(`:white_check_mark: Purged \`${deletedMessages}\` messages.`)
-                    .then(m => m.delete(2000));
-        }).catch(console.error);
+            }).then(() => {
+                    if (deletedMessages === -1) deletedMessages = 0;
+                    message.channel.send(`:white_check_mark: Purged \`${deletedMessages}\` messages.`)
+                        .then(m => m.delete(2000));
+            }).catch(console.error);
+        }
 
+
+
+    // If a message includes any commands from the array, post the value associated with it.
+
+        if (responseObject[message.content]) {
+            message.channel.send(responseObject[message.content]);
+        }
     }
-
-    if(command === "botpurge") {
-        let messagecount = parseInt(args[1]) || 1;
-
-        var deletedMessages = -1;
-
-        message.channel.fetchMessages({limit: Math.min(messagecount + 1, 100)}).then(messages => {
-            messages.forEach(m => {
-                if (m.author.id == 451642426520502273) {
-                    m.delete().catch(console.error);
-                    deletedMessages++;
-                }
-            });
-        }).then(() => {
-                if (deletedMessages === -1) deletedMessages = 0;
-                message.channel.send(`:white_check_mark: Purged \`${deletedMessages}\` messages.`)
-                    .then(m => m.delete(2000));
-        }).catch(console.error);
-    }
-
-
-
-// If a message includes any commands from the array, post the value associated with it.
-
-    if (responseObject[message.content]) {
-        message.channel.send(responseObject[message.content]);
-    }
-
 
 });
 
